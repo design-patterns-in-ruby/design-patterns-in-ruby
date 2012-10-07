19ec29429086f41e4c765064ad6c4f77032c7d06
nslocum <nslocum@gmail.com>
2012-10-07 12:05:29 -0400

    Change some Report methods tobe optional.

--- template_method/Report.rb
+++ template_method/Report.rb
@@ -7,39 +7,29 @@ class Report
   def output_report
     output_start
     output_head
-    output_body_start
-    output_body
-    output_body_end
-    output_end
-  end
-
-  def output_body
     @text.each do |line|
       output_line(line)
     end
+    output_end
   end
 
   def output_start
-    raise 'Called abstract method: output_start'
   end
 
   def output_head
-    raise 'Called abstract method: output_head'
+    output_line(@title)
   end
 
   def output_body_start
-    raise 'Called abstract method: output_body_start'
   end
-
+￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼
   def output_line(line)
     raise 'Called abstract method: output_line'
   end
 
   def output_body_end
-    raise 'Called abstract method: output_body_end'
   end
 
   def output_end
-    raise 'Called abstract method: output_end'
   end
 end
