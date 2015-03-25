084b5f6fcc7eef4b7a4cd10008e030989ec85f7c
Nick Slocum <nslocum@gmail.com>
2015-03-25 12:48:22 -0400

    Updated code to work with ruby 2.2.0.

--- iterator/internal_iterator/for_each_element.rb
+++ iterator/internal_iterator/for_each_element.rb
@@ -1,7 +1,7 @@
 def for_each_element(array)
   # Make a deep copy the array so that changes to the original array
   # wont effect the iterator
-  array_copy = Array.new(array)
+  array_copy = array.clone
   i = 0
   while i < array_copy.length
     yield array_copy[i]
