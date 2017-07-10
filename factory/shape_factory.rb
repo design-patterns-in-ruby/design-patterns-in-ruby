1f65a14b5da44d8312be5168b0a1bd2e6d106b05
rbUUbr <kirabsuir@gmail.com>
2017-07-10 14:00:32 +0300

    Removed empty constructor

--- factory/shape_factory.rb
+++ factory/shape_factory.rb
@@ -3,8 +3,6 @@ require_relative 'rectangle'
 require_relative 'square'
 
 class ShapeFactory
-  def initialize
-  end
   # use getShape method to get object of type shape
   def get_shape(type)
      Object.const_get("#{type.to_s}").new
