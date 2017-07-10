391cff60071d07649fb6374b430a5b6c844bceae
rbUUbr <kirabsuir@gmail.com>
2017-07-10 13:58:29 +0300

    Removed switch/case construction

--- factory/shape_factory.rb
+++ factory/shape_factory.rb
@@ -3,12 +3,10 @@ require_relative 'rectangle'
 require_relative 'square'
 
 class ShapeFactory
+  def initialize
+  end
   # use getShape method to get object of type shape
   def get_shape(type)
-    case type
-    when 'CIRCLE'    then Circle.new
-    when 'RECTANGLE' then Rectangle.new
-    when 'SQUARE'    then Square.new
-    end
+     Object.const_get("#{type.to_s}").new
   end
 end
